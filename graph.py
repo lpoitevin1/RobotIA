6be9f93da11996614e61501f4cfebbdbf7c38ad7
Thierno Bah <thierno-mamoudou.bah@etu.univ-lyon1.fr>
2019-04-29 02:28:53 +0200

    generation graph

--- graph.py
+++ graph.py
@@ -1,32 +1,24 @@
+import csv
 from collections import defaultdict
 
-graph = defaultdict(list) 
 
+ 
 
-def addArc(graph,u,v,d): 
-	sublist=[v,d]
-	graph[u].append(sublist) 
-    
-    
+with open('nodesTest.csv') as fichier:
+	r=csv.reader(fichier)
+	graph = defaultdict(list)
+	for row in r:
+		graph[row[0]].append(row[1:])	    
 
 def generate_Arc(graph):
 	edges=[]
-	
-	
+		
 	for node in graph:
 		
 		for neighbour in graph[node]:
 			edges.append((node,neighbour))
 	return edges
-	
-	
 
-addArc(graph,'a','c','N') 
-addArc(graph,'b','c','S') 
-addArc(graph,'b','e','E') 
-addArc(graph,'c','d','W') 
-addArc(graph,'c','e','S') 
-addArc(graph,'c','a','S') 
 
 
 print(generate_Arc(graph))
